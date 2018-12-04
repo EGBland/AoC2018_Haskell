5a175a19933928bb5bc43dfb4922a0b1823f9540
E. G. Bland <egb528@york.ac.uk>
2018-12-04 01:07:06 +0000

    add first problem for day 2
    i can probably simplify the list comprehensions, but i'll save that for when i'm less tired.

---
+++ day2.hs
@@ -0,0 +1,26 @@
+import System.IO
+import Control.Monad
+
+day2_1 = do
+    contents <- readFile "input_02.txt"
+    let list = words contents
+    let freqs = [(id,[(a,b) | (a,b) <- letterCounts id]) | id <- list]
+    let twos = [(id,hastwos) | idc <- freqs, hastwos <- [getNs (snd idc) 2], id <- [fst idc]]
+    let threes = [(id,hasthrees) | idc <- freqs, hasthrees <- [getNs (snd idc) 3], id <- [fst idc]]
+
+    let checksum = (length [id | two <- twos, snd two]) * (length [id | three <- threes, snd three])
+    putStrLn (show checksum)
+    
+getNs :: [(Char,Int)] -> Int -> Bool
+getNs freqs n
+    | length freqs == 1 = snd (head freqs) == n
+    | otherwise = or [getNs ([head freqs]) n,getNs (tail freqs) n]
+
+letterCounts :: String -> [(Char,Int)]
+letterCounts s = [(letter,freq) | letter <- ['a'..'z'], freq <- [letterCount s letter]]
+
+letterCount :: String -> Char -> Int
+letterCount s c
+    | null s = 0
+    | length s == 1 = if head s == c then 1 else 0
+    | otherwise = letterCount ([head s]) c + letterCount (tail s) c
